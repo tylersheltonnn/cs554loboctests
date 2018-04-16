ab6369bdc047808ba68a7dbc108f831f823e27ec
tylersheltonnn <tshelt0n@unm.edu>
2018-04-16 14:47:44 -0600

    changed the location of all the tests, so they would be more uniform.

--- TS_lobocTests/testRunner.sh
+++
@@ -1,5 +0,0 @@
-for i in {10..40}
-do
-echo is1_$i.loboc
-./tlc -c "java -jar /home/tyler/Desktop/spike5/tlctest/compilerHolder/CompilerFinal.jar /home/tyler/Desktop/spike5/tlctest/is1_$i.loboc" -test is1_$i.loboc 
-done
